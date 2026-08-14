408e53e5660aad845e262b3f3103edcf03ca463b
securecsharptek <securecsharptek@users.noreply.github.com>
2026-08-14 16:22:31 +0530

    test

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,5 +8,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 1,
     },
+    {
+      url: 'https://coder-wala-music.vercel.app/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
   ]
 }
